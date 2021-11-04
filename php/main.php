41bf59816badb887588f30dab6173412f61a45d0
Adam Scoggins <scoggins.aj@gmail.com>
2021-11-04 16:57:53 -0500

    Finished addTask in php

--- php/main.php
+++ php/main.php
@@ -2,26 +2,26 @@
 // This file returns a JSON encoded array as a result
 
     // This is where the code begins execution
-    if (!isset($_POST['action'])) {
+    if (!isset($_GET['action'])) {
         $result = array('error' => 'There is no action in the given request.');
         echo json_encode($result);
         return;
     }
 
-    // Each post request should have an action
+    // Each GET request should have an action
     // The 'add' action is for adding a new task. This requires:
     // Timestamp, day, month, year, time_required, title, and description
-    if ($_POST['action'] == 'add') {
+    if ($_GET['action'] == 'add') {
 
         // If there are missing variables, return an error
-        if (!isset($_POST['timestamp']) || !isset($_POST['day']) || !isset($_POST['month']) || !isset($_POST['year']) ||
-            !isset($_POST['time_required']) || !isset($_POST['title']) || !isset($_POST['description'])) {
+        if (!isset($_GET['timestamp']) || !isset($_GET['day']) || !isset($_GET['month']) || !isset($_GET['year']) ||
+            !isset($_GET['time_required']) || !isset($_GET['title']) || !isset($_GET['description'])) {
                 $result = array('error' => 'There are missing variables.');
                 echo json_encode($result);
                 return;
         }
 
-        addTask($_POST['timestamp'], $_POST['day'], $_POST['month'], $_POST['year'], $_POST['time_required'], $_POST['title'], $_POST['description']);
+        addTask($_GET['timestamp'], $_GET['day'], $_GET['month'], $_GET['year'], $_GET['time_required'], $_GET['title'], $_GET['description']);
     }
     
     function addTask($timestamp, $day, $month, $year, $timeRequired, $title, $description) {
@@ -31,6 +31,13 @@
             // Get the file contents and convert it to JSON
             $fileData = file_get_contents($file);
             $arrayData = json_decode($fileData, true);
+
+            // If there is no data present, create a new array
+            if ($arrayData == null) {
+                $arrayData = array();
+            }
+
+            // Create the new task array
             $newTask = array('timestamp' => $timestamp, 
                                 'day' => $day,
                                 'month' => $month,
