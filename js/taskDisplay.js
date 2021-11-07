40ce03e84796eef24ce195553602b539ca5ec784
Starwell <40969577+Starwell@users.noreply.github.com>
2021-11-06 21:47:07 -0500

    Added task display and updated form onsubmit property.
    Added two functions, mouseOver, and mouseOut that allow for the displaying of tasks.
    Added an example task.
    Updated onsubmit to return addTask().

---
+++ js/taskDisplay.js
@@ -0,0 +1,13 @@
+function mouseOver() {
+  document.getElementById("project1").style.color = "#63ca00";
+  document.getElementById("project1").innerHTML =
+  "Example title<br>A project.<br>10 hours to completion";
+}
+function mouseOut() {
+  document.getElementById("project1").style.color = "black";
+  document.getElementById("project1").innerHTML =
+  "1<br>Example Title";
+}
+function assignTask() {
+
+}
