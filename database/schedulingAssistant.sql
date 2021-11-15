a68501b72fda718e4ba9d2a7b698320ddd899d12
ray french <58053063+rayfrench@users.noreply.github.com>
2021-11-15 23:15:55 +0500

    Update schedulingAssistant.sql
    Updated the database with a new table tasks to store the tasks being inserted by the users

--- database/schedulingAssistant.sql
+++ database/schedulingAssistant.sql
@@ -2,10 +2,29 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 --
 -- Database: `schedulingAssistant`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `taskid` int(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `timeRequired` decimal(10,2) NOT NULL,
+  `day` int(10) NOT NULL,
+  `month` int(10) NOT NULL,
+  `year` int(10) NOT NULL,
+  `completed` int(100) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 
 -- --------------------------------------------------------
 
@@ -19,11 +38,20 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `users`
+--
 
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`taskid`);
 
 --
 -- Indexes for table `users`
@@ -36,10 +64,14 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `taskid` int(100) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-
